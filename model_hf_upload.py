2833db90bfd4241862aff6e66c2fe3421d92b0df
Shane Martin <martin.816@wright.edu>
2026-08-10 14:33:47 -0400

    Delete model_hf_upload.py

--- model_hf_upload.py
+++
@@ -1,8 +0,0 @@
-from sentence_transformers import SentenceTransformer
-
-# 1. Load your local model folder
-model = SentenceTransformer("./fine_tuned_osrs_embedder_v2")
-
-# 2. Push to your Hugging Face account
-# Format: "your-hf-username/repository-name"
-model.push_to_hub("shmartcode/osrs-embedder-v2", private=False)
